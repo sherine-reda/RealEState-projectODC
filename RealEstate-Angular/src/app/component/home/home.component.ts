6b0d2675e090dd5d31b1b97a8072cb111545cc0d
Sherine <83099016+sherine-reda@users.noreply.github.com>
2023-01-24 01:10:18 +0200

    Add files via upload

---
+++ RealEstate-Angular/src/app/component/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+	images = ['assets/img/7.jpg','assets/img/8.jpg','assets/img/9.jpg'];
+// 1366/535
+}
